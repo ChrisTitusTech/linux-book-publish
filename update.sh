e7332ee22145bdcc49c1351d8271ca2fefe78cc2
Chris Titus <contact@christitus.com>
2025-06-14 12:17:57 -0500

    Update book content

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+mdbook build /home/titus/github/linux-book
 rsync -avP /home/titus/github/linux-book/book/html/* /home/titus/github/linux-book-publish/
 git add .
 git commit -m "Update book content"
