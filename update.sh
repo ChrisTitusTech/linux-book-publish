066f5eea6077ef407fe08dfdeac24145b7cb72e2
Chris Titus <contact@christitus.com>
2025-09-24 16:41:43 -0500

    add macos func

--- update.sh
+++ update.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-mdbook build /home/titus/github/linux-book
-rsync -avP /home/titus/github/linux-book/book/html/* /home/titus/github/linux-book-publish/
+
+# Detect operating system and set appropriate home path
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    HOME_PATH="/home/titus"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    HOME_PATH="/Users/titus"
+else
+    echo "Unsupported operating system: $OSTYPE"
+    exit 1
+fi
+
+mdbook build "${HOME_PATH}/github/linux-book"
+rsync -avP "${HOME_PATH}/github/linux-book/book/html/" "${HOME_PATH}/github/linux-book-publish/"
 git add .
 git commit -m "Update book content"
 git push
